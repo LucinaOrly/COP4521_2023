161996dcb754cd6fbd8f2de452cd5da3f1810d9b
John Valencia-Londono <l.orly528@outlook.com>
2023-09-24 17:22:35 -0400

    sync to GitHub

--- Module4/HospitalApp.py
+++ Module4/HospitalApp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 con = sql.connect('HospitalDB.db')
 con.execute('''CREATE TABLE IF NOT EXISTS hospital(
-            Name TEXT NOT NULL,
+            Name TEXT NOT NULL PRIMARY KEY,
             Age INTEGER NOT NULL,
             Phone TEXT NOT NULL,
             COVID INTEGER NOT NULL,
@@ -25,25 +25,45 @@ def new_patient():
 @app.route('/input',methods = ['post', 'get'])
 def addrec():
     if request.method == 'POST':
+        msgs = []
+
         try:
             nm = request.form['name']
-            print(nm)
-            ag = request.form['age']
-            print(ag)
-            ph = request.form['phone']
-            print(ph)
-            co = request.form.get('covid')
-            print(co)
-            sc = request.form['security']
-            print(sc)
-            pw = request.form['password']
-            print(pw)
+            if nm == '':
+                msgs.append("You can not enter in an empty name")
 
-            print("load success")
-            with sql.connect("hospitaldb.db") as con:
+            ag = request.form['age']
+            if not isinstance(ag, int) or ag < 0 or ag > 121:
+                msgs.append("The Age must be a whole number greater than 0 and less than 121.")
+
+
+            ph = request.form['phone']
+            if ph == '':
+                msgs.append("You can not enter in an empty phone number")
+
+            co = request.form.get('covid')
+            if co == 'on':
+                co = 1
+            else:
+                co = 0
+
+            sc = request.form['security']
+            if not isinstance(sc, int) or sc < 1 or sc > 3:
+                msgs.append("The SecurityRoleLevel must be a numeric between 1 and 3.")
+
+            pw = request.form['password']
+            if pw == '':
+                msgs.append("You can not enter in an empty pwd")
+
+            # If any errors in the input, raise exception and write messages to user
+            if len(msgs) > 0:
+                raise Exception("Error messages present")
+
+            # write requests to database
+            with sql.connect("hospitalDB.db") as con:
                 cur = con.cursor()
 
-                cur.execute("insert into hospital (name,age,phone,covid,security,password) values (?,?,?,?,?,?)",
+                cur.execute("INSERT INTO hospital (name,age,phone,covid,security,password) VALUES (?,?,?,?,?,?)",
                                                 nm,ag,ph,co,sc,pw)
 
                 con.commit()
@@ -51,9 +71,13 @@ def addrec():
         except:
             print("excepted")
             con.rollback()
-            msg = "error in insert operation"
+
+            # print all messages
+            for m in msgs:
+                msg += m + '\n'
 
         finally:
+            print(msg)
             return render_template("result.html", msg = msg)
             con.close()
 
