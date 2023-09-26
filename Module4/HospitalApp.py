b54dc86e7bec1450915dfcc67c46722a2cecae89
John Valencia-Londono <l.orly528@outlook.com>
2023-09-25 20:02:26 -0400

    note that the error messages need to be reformatted to have new line delim

--- Module4/HospitalApp.py
+++ Module4/HospitalApp.py
@@ -1,3 +1,58 @@
+"""
+Name:John Valencia-Londono
+Date:09/25/2023
+Assignment:Module 4: Basic Flask Website
+Due Date:09/24/2023
+About this project:
+    Develop a frontend application that interacts with a database for a small scale real-world applications using third-party Python libraries discussed in the course.
+    Solve a simple programming problem based on various approaches to computer security and information management.
+    Directions:
+    Using Python and the Flask library, create a Flask website with the following pages
+
+    Home
+    Add a new Hospital App User
+    List Hospital App Users
+    Results
+    The Home page
+    Should have
+
+    Link - Add a new Hospital App User - (opens the add a new Hospital App User page)
+    Link - List Hospital App Users - (opens the list Hospital App Users page)
+    The Add new Hospital App User page
+    Should have
+
+    a label and input text field for each possible attribute other than the id and Has COVID
+    a label and input checkbox field for Has COVID
+    Submit button -
+    When the Submit button is clicked:
+
+    Validates the values entered by the user.
+
+    If the values are valid then a record is added to the HospitalUser table with the values entered by the user and a record added message is sent to the result page to display to the user.
+
+    Otherwise, an error message is created indicating all the input errors. This message is sent to the result page to display to the user.)
+
+    Input Validation Rules:
+
+    the Name is not empty and does not only contain spaces
+    the Age is a whole number greater than 0 and less than 121 (Hint: Python - How to validate that a variable stores a numeric valueLinks to an external site.)
+    the PhoneNumber is not empty and does not only contain spaces
+    The SecurityRoleLevel must be a numeric between 1 and 3. (Hint: Python - How to validate that a variable stores a numeric valueLinks to an external site.)
+    the LoginPassword is not empty and does not only contain spaces
+    The List Hospital App Users page
+    Should have
+
+    a table that displays the following information for every record in the HospitalUser table:
+    UserName, UserAge, UserPhNum, UserHasCOVID, UserSecurityLevel, and LoginPassword
+
+    Link - Go back to home page - (opens the Home page)
+    The Results page
+    Should have
+
+    display the value of the variable msg (Note this message could be a recorded added message or an input validation error record not added message)
+    Link - Go back to home page - (opens the Home page)
+Assumptions: N/A
+All work below was performed by John Valencia-Londono """
 from sqlite3 import Connection
 
 from flask import Flask, render_template, request
@@ -35,30 +90,38 @@ def addrec():
         msgs = []
 
         try:
+            # not empty
+            # remove trailing and leading whitespace from input
             nm = request.form['name']
             if nm == '':
                 msgs.append("You can not enter in an empty name")
 
+            # must be from 0 to 121
             ag = request.form['age']
             if not str.isdigit(ag) or int(ag) < 0 or int(ag) > 121:
-                print( str.isdigit(ag))
+                print(str.isdigit(ag))
                 msgs.append("The Age must be a whole number greater than 0 and less than 121.")
 
+            # not empty
+            # remove trailing and leading whitespace from input
             ph = request.form['phone']
             if ph == '':
                 msgs.append("You can not enter in an empty phone number")
 
+            # a checkbox that returns 'on' or 'off'
             co = request.form.get('covid')
             if co == 'on':
                 co = 1
             else:
                 co = 0
 
+            # must be from 1 to 3
             sc = request.form['security']
             if not str.isdigit(sc) or int(sc) < 1 or int(sc) > 3:
                 msgs.append("The SecurityRoleLevel must be a numeric between 1 and 3.")
 
-            pw = request.form['password']
+            # remove trailing and leading whitespace from input
+            pw = request.form['password'].strip()
             if pw == '':
                 msgs.append("You can not enter in an empty pwd")
 
@@ -70,22 +133,18 @@ def addrec():
             cur = con.cursor()
 
             cur.execute("INSERT INTO hospital (name,age,phone,covid,security,password) VALUES (?,?,?,?,?,?)",
-                            (nm, ag, ph, co, sc, pw))
+                        (nm, ag, ph, co, sc, pw))
 
             con.commit()
             msgs.append("record successfully added")
         except:
             print("excepted")
             con.rollback()
-
-            # print all messages
-            msg = "<br>"
-            msg.join(msgs)
         finally:
             print(msgs)
             con.close()
-            msg = "\n"
-            return render_template("result.html", msg=msg.join(msgs))
+            msg = "<br>"
+            return render_template("result.html", msg='<br>' + msg.join(msgs))
 
 
 @app.route('/list')
