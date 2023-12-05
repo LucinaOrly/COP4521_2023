aaedf17b3b9b7ff96014f148f104ef256c1eb0b1
John Valencia-Londono <l.orly528@outlook.com>
2023-12-04 23:54:36 -0500

    Finished mod14

--- Module14/mod14_viz.py
+++ Module14/mod14_viz.py
@@ -1,14 +1,3 @@
-"""
-https://catalog.data.gov/dataset/motor-vehicle-collisions-crashes
-Name:John Valencia-Londono
-Date:12/4/2023
-Assignment:Module14: Dask Large Dataset
-Due Date:12/3/2023
-About this project:
-Demonstrate knowledge of distributed computing by using the Dask library to compute
-Data Sets and fetch Series to compute aggregates. Also visualize a DAG used by DASK
-All work below was performed by John Valencia-Londono
-"""
 import time
 from dask.diagnostics import ProgressBar
 import dask.dataframe as ddf
@@ -29,8 +18,7 @@ df = ddf.read_csv(CSV, dtype=dtype,assume_missing=True)
 time_elapsed = time.time()-time_start
 print(f"Time elapsed: {time_elapsed}ms")
 
-with ProgressBar():
-       print(df.dtypes)
+with ProgressBar(): print(df.dtypes)
 
 
 
@@ -38,34 +26,34 @@ print()
 print("Aggregate for NUMBER OF PERSONS INJURED:")
 series = df["NUMBER OF PERSONS INJURED"]
 with ProgressBar():
-       print(series.describe().apply(lambda x: format(x,'f'), meta=('NUMBER OF PERSONS INJURED', 'float64')).compute())
+       print(series.describe().visualize("NUMBER OF PERSONS INJURED"))
 
 print()
 print("Aggregate for NUMBER OF PERSONS KILLED:")
 series = df["NUMBER OF PERSONS KILLED"]
 with ProgressBar():
-       print(series.describe().apply(lambda x: format(x,'f'), meta=('NUMBER OF PERSONS INJURED', 'float64')).compute())
+       print(series.describe().visualize("NUMBER OF PERSONS KILLED"))
 
 print()
 print("Aggregate for VEHICLE TYPE CODE 1:")
 series = df["VEHICLE TYPE CODE 1"]
 with ProgressBar():
-       print(series.describe().compute())
-
+       print(series.describe().visualize("VEHICLE TYPE CODE 1"))
+       
 print()
 print("Aggregate for VEHICLE TYPE CODE 3:")
 series = df["VEHICLE TYPE CODE 3"]
 with ProgressBar():
-       print(series.describe().compute())
+       print(series.describe().visualize("VEHICLE TYPE CODE 3"))
 
 print()
 print("Aggregate for CONTRIBUTING FACTOR VEHICLE 3:")
 series = df["CONTRIBUTING FACTOR VEHICLE 3"]
 with ProgressBar():
-       print(series.describe().compute())
+       print(series.describe().visualize("CONTRIBUTING FACTOR VEHICLE 3"))
        
 print()
 print("Aggregate for ZIP CODE:")
 series = df["ZIP CODE"]
 with ProgressBar():
-       print(series.describe().compute())
+       print(series.describe().visualize("ZIP CODE"))
